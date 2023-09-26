560fe2e56e65a5dd6695c66b191d580873e83708
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2023-09-26 19:45:37 +0200

    update script formating cours

--- md.jsx
+++ md.jsx
@@ -1,50 +1,144 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { tomorrowNight as dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-function MarkdownComponent() {
-  const markdownText = `
-  # Titre principal
+const FormatageCours = ({ cours }) => {
+  const [coursSplit, setCoursSplit] = useState([]);
 
-  Voici un exemple de code JSX :
-  `;
+  useEffect(() => {
+    const test = cours.split("[jsx]");
+    setCoursSplit(test);
+  }, [cours]);
 
   return (
-    <div>
-      <h1>Contenu Markdown</h1>
-      <ReactMarkdown>{markdownText}</ReactMarkdown>
-    </div>
+    <>
+      {coursSplit.map((e, i) => {
+        if (i % 2 === 0) {
+          return <MarkdownComponent markdownText={e} />;
+        } else {
+          return <FormatCode code={e} />;
+        }
+      })}
+    </>
   );
+};
+
+function MarkdownComponent({ markdownText }) {
+  return <ReactMarkdown>{markdownText}</ReactMarkdown>;
 }
 
-const YourComponent = () => {
-  const code = `
-    function App() {
-      return (
-        <div className="App">
-          <h1>Hello, React!</h1>
-        </div>
-      );
-    }
-  `;
-
+const FormatCode = ({ code }) => {
   return (
-    <div>
-      <h2>Code JSX avec SyntaxHighlighter</h2>
-      <SyntaxHighlighter language="javascript" style={docco}>
-        {code}
-      </SyntaxHighlighter>
-    </div>
+    <SyntaxHighlighter language="javascript" style={dark}>
+      {code}
+    </SyntaxHighlighter>
   );
 };
 
 export default function App() {
+  const test = `
+  # Cours sur l'Introduction à JSX en React
+
+  ## Introduction
+
+JSX (JavaScript XML) est une extension de JavaScript qui permet de définir la structure et l'apparence des éléments dans les composants React. Il offre une manière élégante de créer des interfaces utilisateur réactives en combinant JavaScript et HTML de manière syntaxiquement familière. Dans ce cours, nous allons explorer ce qu'est JSX, comment l'utiliser dans les composants React, et pourquoi il est essentiel pour le développement web moderne.
+
+## Table des matières
+
+1. [Introduction à JSX](#introduction-à-jsx)
+2. [Syntaxe de Base de JSX](#syntaxe-de-base-de-jsx)
+3. [Intégration de Variables](#intégration-de-variables)
+4. [Expressions JavaScript dans JSX](#expressions-javascript-dans-jsx)
+5. [Utilisation de JSX dans React](#utilisation-de-jsx-dans-react)
+6. [Avantages de JSX](#avantages-de-jsx)
+7. [Conclusion](#conclusion)
+
+## 1. Introduction à JSX
+
+JSX est une syntaxe de langage de balisage similaire à HTML qui est utilisée dans React pour définir la structure des composants React. C'est une extension de JavaScript qui permet aux développeurs de créer des éléments React de manière plus intuitive.
+
+## 2. Syntaxe de Base de JSX
+
+En JSX, les éléments HTML sont définis à l'intérieur de parenthèses et sont généralement inclus dans une seule balise parente.
+
+[jsx]
+// Exemple de JSX
+const element = (
+  <div>
+    <h1>Titre</h1>
+    <p>Paragraphe de texte.</p>
+  </div>
+);
+[jsx]
+
+Dans cet exemple, nous avons créé un élément React \`element\` qui contient une \`div\` avec un \`h1\` et un \`p\` à l'intérieur.
+
+## 3. Intégration de Variables
+
+Vous pouvez intégrer des variables JavaScript dans JSX en les plaçant entre accolades \`{}\`.
+
+[jsx]
+// Exemple de JSX avec variable
+const nom = "John";
+const element = <p>Bonjour, {nom} !</p>;
+[jsx]
+
+Dans cet exemple, la variable \`nom\` est insérée dans le texte du paragraphe.
+
+## 4. Expressions JavaScript dans JSX
+
+Vous pouvez également utiliser des expressions JavaScript dans JSX en les plaçant entre accolades \`{}\`.
+
+[jsx]
+// Exemple d'expression JavaScript dans JSX
+const x = 10;
+const y = 5;
+const element = (
+  <p>
+    La somme de {x} et {y} est {x + y}.
+  </p>
+);
+[jsx]
+
+Dans cet exemple, nous utilisons des expressions JavaScript pour effectuer une addition à l'intérieur du texte du paragraphe.
+
+## 5. Utilisation de JSX dans React
+
+Dans React, vous pouvez utiliser JSX pour définir la structure des composants. Les composants React sont généralement définis comme des fonctions ou des classes qui retournent du JSX.
+
+[jsx]
+// Exemple de composant React avec JSX
+function MonComposant() {
+  return (
+    <div>
+      <h1>Mon Composant React</h1>
+      <p>Ceci est un composant React.</p>
+    </div>
+  );
+}
+[jsx]
+
+Dans cet exemple, nous avons créé un composant React \`MonComposant\` qui renvoie du JSX pour définir sa structure.
+
+## 6. Avantages de JSX
+
+- **Lisible et familier :** La syntaxe JSX ressemble à HTML, ce qui la rend plus facile à lire et à comprendre.
+- **Intégration de données :** Vous pouvez insérer des données dynamiques directement dans le balisage JSX en utilisant des accolades \`{}\`.
+- **Optimisation de la performance :** React peut optimiser les mises à jour du DOM grâce à la réconciliation virtuelle basée sur JSX.
+
+## 7. Conclusion
+
+JSX est un élément essentiel du développement d'applications React. Il offre une manière élégante de définir l'apparence et la structure de vos composants, tout en permettant l'intégration de données dynamiques. En comprenant JSX, vous serez mieux préparé pour développer des interfaces utilisateur réactives avec React.
+
+C'est la fin de ce cours sur l'introduction à JSX en React. J'espère que vous avez acquis une compréhension solide de cette syntaxe et que vous serez en mesure de l'appliquer avec succès dans vos projets React.
+
+  `;
   return (
     <div className="App">
-      <MarkdownComponent />
-      <YourComponent />
+      {/* <MarkdownComponent />
+      <FormatCode /> */}
+      <FormatageCours cours={test} />
     </div>
   );
 }
